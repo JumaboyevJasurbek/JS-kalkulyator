9f9664dfa4ff0ea79a7d22f4b498917b5bd8a240
JumaboyevJasurbek <100076086+JumaboyevJasurbek@users.noreply.github.com>
2022-04-06 14:52:09 +0500

    konkulyator

--- main.js
+++ main.js
@@ -1,27 +1,38 @@
-let son = parseInt(prompt("Birinchi sonni kiriting"));
+let num = prompt("sonni kiriting");
 
-let amal = prompt("Amalni kiriting");
-
-let son2 = parseInt(prompt("Ikinchi sonni kiriting"));
-
-
-if (isNaN(son) || isNaN(son2)) {
-    console.log("Raqam kiritilmagan");
-}else{
-
-    if (amal == "+") {
-        console.log("Natijangiz " + (son+son2) );
-
-    }else if (amal == "/") {
-        console.log("Natijangiz " + (son/son2));
-
-    }else if(amal == "*") {
-        console.log("Natijangiz " + (son*son2));
-
-    }else if(amal == "-") {
-        console.log("Natijangiz " + (son-son2));
-        
-    }else {
-        console.log("Xatolik bor boshqatdan urinib koring !");
-    }
+for (let i = 1; i < 10; i++) {
+    num *= i
+    console.log(num);
 }
+
+
+
+
+
+// let son = parseInt(prompt("Birinchi sonni kiriting"));
+
+// let amal = prompt("Amalni kiriting");
+
+// let son2 = parseInt(prompt("Ikinchi sonni kiriting"));
+
+
+// if (isNaN(son) || isNaN(son2)) {
+//     console.log("Raqam kiritilmagan");
+// }else{
+
+//     if (amal == "+") {
+//         console.log("Natijangiz " + (son+son2) );
+
+//     }else if (amal == "/") {
+//         console.log("Natijangiz " + (son/son2));
+
+//     }else if(amal == "*") {
+//         console.log("Natijangiz " + (son*son2));
+
+//     }else if(amal == "-") {
+//         console.log("Natijangiz " + (son-son2));
+        
+//     }else {
+//         console.log("Xatolik bor boshqatdan urinib koring !");
+//     }
+// }
